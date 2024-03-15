ed8d18c7a50cee2de35ffe1726d76d268b000f11
Maryam-Eid <maryam79ah@gmail.com>
2024-03-15 23:26:44 +0200

    global scope

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected static function booted()
+    {
+        static::addGlobalScope('store', function (Builder $builder) {
+            if (Auth::user()->store_id) {
+                $builder->where('store_id', Auth::user()->store_id);
+            }
+        });
+    }
 }
